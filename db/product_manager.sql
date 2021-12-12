3b196ccb1bb635296110a49b2d5b0903135cbe82
ccx1010sky <ccx1010@gmail.com>
2021-12-12 21:33:12 +0000

    run_sql.py and sql file

---
+++ db/product_manager.sql
@@ -0,0 +1,23 @@
+-- 
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS manufs;
+
+CREATE TABLE manufs (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    info VARCHAR(255)
+);
+
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description VARCHAR(255),
+  stock_quanity INT,
+  cost INT,
+  selling_price INT 
+  mark_up INT
+  manuf_id REFERENCES manufs(id) 
+  
+);
+
--- models/product.py
+++ models/product.py
@@ -1,10 +1,9 @@
 class Product:
-    def __init__(self,name,description,stock_quantity,cost,selling_price,markup,id = None):
+    def __init__(self,name,description,stock_quantity,cost,selling_price,mark_up,id = None):
         self.name = name
         self.description = description
         self.stock_quantity = stock_quantity
         self.cost = cost
         self.selling_price = selling_price
-        self.markup = markup
+        self.mark_up = mark_up
         self.id = id
-        
