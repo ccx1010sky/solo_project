609d2cce8fcedc1f591438ba7bac200fdf0ef140
ccx1010sky <ccx1010@gmail.com>
2021-12-13 14:08:35 +0000

    edit products/index.html

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
-
+# 
 from controllers.products_controller import products_blueprint
+
 app = Flask(__name__)
 
+# 
 app.register_blueprint(products_blueprint)
 
 @app.route('/')
